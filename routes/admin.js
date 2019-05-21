2fe97a987c39cc5d57b57f064f2deda89dc3ff84
igor silva <silvaigs.is@gmail.com>
2019-05-21 17:21:37 -0300

    descomentado arquivo de rota de direcionamento

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const login = require("../controller/controller_login");
 const looping_ = require("../utils/looping");
 const looping = new looping_()
 
-/*
+
 router.use(function(req,res,next){
   if(['/login',"/admin/looping/","/admin/start/"].indexOf(req.url) === -1 && !req.session.user){
 
@@ -17,7 +17,7 @@ router.use(function(req,res,next){
   }
 
 
-});*/
+});
 
 
 
