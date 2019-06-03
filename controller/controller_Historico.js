14c38a542762a79f999c34e11f1f05b457f040cb
isilva-igs <silvaigs.is@gmail.com>
2019-06-03 00:39:41 -0300

    tela historico finalizada

--- controller/controller_Historico.js
+++ controller/controller_Historico.js
@@ -3,15 +3,15 @@ const repositorio_Historico = require('../repositorio/repositorio_Historico')
 module.exports= {
 
   select(dataInit,dataEnd,sensor){
-    return new Promise((resolve,reject)=>{
+    return new Promise(  (resolve,reject)=>{
         
         if(!dataInit){
             dataInit='2019-05-01'
             dataEnd='2019-05-30'
             sensor ='2'
         }
-        repositorio_Historico.select(dataInit,dataEnd,sensor).then(rs=>{
-            rs = this.tratarDados(rs);
+        repositorio_Historico.select(dataInit,dataEnd,sensor).then( async rs=>{
+            rs = await this.tratarDados(rs);
             resolve(rs);
         })
 
@@ -19,9 +19,35 @@ module.exports= {
   },
   tratarDados(rs){
     rs.forEach(element => {
-      var dado = element.tempMedia
-      element = parseFloat(dado.toFixed(2));
+      var dado="";
+      var dado2="";
+    
+      if(typeof element.tempMedia == 'object'){
+         dado = element.tempMedia[0]
+      }else{
+        dado = element.tempMedia;
+      }
+
+      if(typeof element.umidMedia == 'object'){
+        dado2 = element.umidMedia[0]
+     }else{
+       dado2 = element.umidMedia;
+     }
+
+     var data = new Date(element.data );
+      data = (data.getDate())+'/'+(data.getMonth()+1)+'/'+data.getFullYear()
+
+      element.data = data;
+      element.tempMedia = parseFloat(dado.toFixed(2));
+      element.umidMedia = parseFloat(dado2.toFixed(2));
+
+
+
     });
+
+    
+    
+    return rs;
   }
 }
 
